d4b1dd4fcf0d7e1ebe8f83a2ae2217847b46fe06
hphanixa <77008970+hphanixa@users.noreply.github.com>
2022-05-20 16:50:27 +0200

    Update ft_putchar.c

--- ft_putchar.c
+++ ft_putchar.c
@@ -14,7 +14,7 @@
 
 /* la fonction renvoie un ssize_t car l'appel a ft_putchar_fd renvoie un ssize_t).
 ** On recoit un const char car cette variable n'est pas amenée a etre modifiée.
-/*
+*/
 
 ssize_t	ft_putchar(const char c) 
 {
